b8d6d89422eb6c135d4ade9d1d46795fe547e6bc
koolleeo <leigh.ally@gmail.com>
2023-06-25 15:11:32 +0100

    required https and dotenv to handle API calls and hide variables. set up app.post request to send data JSON to mailchimp API endpoint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const https = require('https');
+const donenv = require('dotenv').config();
 
 // initialise express instance
 const app = express();
@@ -19,12 +21,55 @@ app.get('/', function(req, res){
 // capture post requests from form
 app.post('/', function(req, res){
 
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email)
-    
+    // capture data from body of form >> POST
+    const data = {
+
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+// use JSON.stringify to convert json to string before POST
+const jsonData = JSON.stringify(data);
+
+// define URL
+const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.LISTID}`;
+
+// define options to pass as a parameter
+const options = {
+    method: "POST",
+    auth: `koolleeo:${process.env.APIKEY}`
+}
+
+// make HTTPS request and save as const
+const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+        const parsedData = JSON.parse(data);
+        
+        //log to console to validate 
+        console.log(parsedData);
+
+        // log to page to validate within browser
+        res.send(parsedData);
+
+    })
+})
+
+// POST input jsonData to mailchimp
+request.write(jsonData);
+request.end();
+
 })
 
 app.listen(3000, function(){
